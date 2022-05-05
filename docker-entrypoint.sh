4553924758dc419c1c513c6894d2bc68b7f42144
slominskir <ryan.slominski@gmail.com>
2022-05-05 17:41:29 -0400

    Just sleep

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-echo "----------------------------------------------"
-echo "Step 1: Waiting for a JAWS Schema in Registry "
-echo "----------------------------------------------"
-url=$SCHEMA_REGISTRY
-echo "waiting on: $url"
-while [ $(curl -s -o /dev/null -w %{http_code} $url/subjects/alarm-overrides-value/versions) -ne 200 ] ; do
-  echo -e $(date) " Kafka Registry listener HTTP state: " $(curl -s -o /dev/null -w %{http_code} $url/subjects/alarm-overrides-value/versions) " (waiting for 200)"
-  sleep 5
-done
-
 sleep infinity
